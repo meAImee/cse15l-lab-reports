8523e332910e980b10b8bb8c263aa7e18c1ac510
meAImee <114279586+meAImee@users.noreply.github.com>
2022-10-06 09:39:24 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> stringList;
+
+    public String stringTrack(URI url) {
+        if (url.getPath().contains("/add?s=")) {
+            stringList.add()
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
